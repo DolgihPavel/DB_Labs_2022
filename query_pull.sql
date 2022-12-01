f1c920d661dc37a2c15147a7af0398ed04dd567a
DolgihPavel <dolgihgod@mail.ru>
2022-12-01 14:16:30 +0300

    modified lab3

--- query_pull.sql
+++ query_pull.sql
@@ -1,3 +1,5 @@
+USE books_store;
+
 SELECT * FROM users
 
 SELECT id,
@@ -35,6 +37,7 @@ FROM books
 ORDER BY TotalSum
 
 SELECT * FROM reviews
+
 SELECT (SELECT name FROM books WHERE books.id=reviews.book_id) AS book,
 (SELECT email FROM users WHERE users.id=reviews.user_id) AS user_email,
 review
@@ -48,3 +51,30 @@ SELECT * FROM book_images
 
 SELECT * FROM book_images
 WHERE url NOT LIKE 'https:%'
+
+SELECT * FROM books
+WHERE price = (SELECT MIN(price) FROM books)
+
+SELECT name,price FROM books
+WHERE price > (SELECT AVG(price) FROM books)
+
+SELECT email,password,
+(SELECT name FROM roles WHERE roles.id=users.role) AS role_name
+FROM users
+WHERE id NOT IN (SELECT user_id FROM Orders)
+
+SELECT * FROM books
+WHERE Price < (SELECT Price FROM books WHERE name='Hamlet')
+
+SELECT id,email
+FROM users
+WHERE EXISTS (SELECT * FROM orders WHERE Orders.user_id = users.id)
+ORDER BY id
+
+SELECT * FROM books
+WHERE NOT EXISTS (SELECT * FROM books_orders WHERE books.id = books_orders.book_id)
+
+SELECT (SELECT name FROM genres WHERE genres.id=genre_id) AS GenreName,
+COUNT(*) AS GenresCount
+FROM books
+GROUP BY genre_id
